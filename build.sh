2c7afcdb2b09be5038a14df6b96f9f2b7c2b7017
James Swineson <github@public.swineson.me>
2018-10-19 17:52:37 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,15 +3,17 @@ set -euv
 
 # install packages
 sudo apt-get update -y
-sudo apt-get install -y git-core cmake build-essential libmicrohttpd-dev libssl-dev libhwloc-dev liblzma-dev tree
+sudo apt-get install -y git-core cmake build-essential libmicrohttpd-dev libssl-dev libhwloc-dev liblzma-dev
 
 # get source
 cd $BUILD_SOURCESDIRECTORY
 git clone https://github.com/fireice-uk/xmr-stak.git xmr-stak
 sed -ie "s/2\.0/0\.0/g" xmr-stak/xmrstak/donate-level.hpp
 
+# compile
 cd $BUILD_BINARIESDIRECTORY
 cmake $BUILD_SOURCESDIRECTORY/xmr-stak -DXMR-STAK_COMPILE=generic -DCMAKE_LINK_STATIC=ON -DCMAKE_BUILD_TYPE=Release -DMICROHTTPD_ENABLE=ON -DOpenSSL_ENABLE=ON -DCPU_ENABLE=ON -DHWLOC_ENABLE=ON -DOpenCL_ENABLE=OFF -DCUDA_ENABLE=OFF
 make -j
 
-tree $BUILD_BINARIESDIRECTORY
+# stage artifacts
+mv $BUILD_BINARIESDIRECTORY/bin $BUILD_ARTIFACTSTAGINGDIRECTORY
