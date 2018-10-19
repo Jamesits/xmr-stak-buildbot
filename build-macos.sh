78e249e697e7798eba4ea5f9d6cd0e08a7aaa4d8
James Swineson <github@public.swineson.me>
2018-10-19 19:03:39 +0800

    Update build-macos.sh

--- build-macos.sh
+++ build-macos.sh
@@ -12,7 +12,8 @@ git clone https://github.com/fireice-uk/xmr-stak.git xmr-stak
 sed -ie "s/2\.0/0\.0/g" xmr-stak/xmrstak/donate-level.hpp
 
 # compile
-cmake . -DOPENSSL_ROOT_DIR=/usr/local/opt/openssl -DXMR-STAK_COMPILE=generic -DCMAKE_LINK_STATIC=ON -DCMAKE_BUILD_TYPE=Release -DMICROHTTPD_ENABLE=ON -DOpenSSL_ENABLE=ON -DCPU_ENABLE=ON -DHWLOC_ENABLE=ON -DOpenCL_ENABLE=ON -DCUDA_ENABLE=OFF
+cd $BUILD_BINARIESDIRECTORY
+cmake $BUILD_SOURCESDIRECTORY/xmr-stak -DOPENSSL_ROOT_DIR=/usr/local/opt/openssl -DXMR-STAK_COMPILE=generic -DCMAKE_LINK_STATIC=ON -DCMAKE_BUILD_TYPE=Release -DMICROHTTPD_ENABLE=ON -DOpenSSL_ENABLE=ON -DCPU_ENABLE=ON -DHWLOC_ENABLE=ON -DOpenCL_ENABLE=ON -DCUDA_ENABLE=OFF
 make -j
 
 # stage artifacts
