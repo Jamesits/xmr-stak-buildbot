f8e8ff62d3612dd8955da6c3e34ae6855c29de4f
James Swineson <github@public.swineson.me>
2018-10-19 21:25:23 +0800

    Update build-windows.ps1

--- build-windows.ps1
+++ build-windows.ps1
@@ -23,3 +23,4 @@ Set-Location $env:BUILD_BINARIESDIRECTORY
 # collect artifacts
 Copy-Item $env:BUILD_BINARIESDIRECTORY\Bin\Release\* $env:BUILD_ARTIFACTSTAGINGDIRECTORY
 Copy-Item $env:BUILD_SOURCESDIRECTORY\xmr-stak-dep\openssl\bin\* $env:BUILD_ARTIFACTSTAGINGDIRECTORY
+Compress-Archive -Path "$env:BUILD_ARTIFACTSTAGINGDIRECTORY\*.exe", "$env:BUILD_ARTIFACTSTAGINGDIRECTORY\*.dll" -CompressionLevel Optimal -DestinationPath "$env:BUILD_ARTIFACTSTAGINGDIRECTORY\xmr-stak.zip"
