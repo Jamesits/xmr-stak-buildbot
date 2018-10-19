c41d651e93eef97cc0813f2afc7c10c56eb1ad78
James Swineson <github@public.swineson.me>
2018-10-19 19:50:48 +0800

    Update build-windows.ps1

--- build-windows.ps1
+++ build-windows.ps1
@@ -12,9 +12,9 @@ git clone https://github.com/fireice-uk/xmr-stak.git xmr-stak
 Get-Content xmr-stak/xmrstak/donate-level.hpp | %{$_ -replace "2\.0", "0.0"} | Out-File -FilePath xmr-stak/xmrstak/donate-level.hpp -Encoding utf8
 
 # compile
-. "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" amd64 -vcvars_ver=14.11
+. "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" amd64 10.0.15063.0 -vcvars_ver=14.11
 . "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsMSBuildCmd.bat"
-set CMAKE_PREFIX_PATH="$env:BUILD_SOURCESDIRECTORY\xmr-stak-dep\hwloc;$env:BUILD_SOURCESDIRECTORY\xmr-stak-dep\libmicrohttpd;$env:BUILD_SOURCESDIRECTORY\xmr-stak-dep\openssl"
+$env:CMAKE_PREFIX_PATH="$env:BUILD_SOURCESDIRECTORY\xmr-stak-dep\hwloc;$env:BUILD_SOURCESDIRECTORY\xmr-stak-dep\libmicrohttpd;$env:BUILD_SOURCESDIRECTORY\xmr-stak-dep\openssl"
 
 cd $env:BUILD_BINARIESDIRECTORY
 cmake -G "Visual Studio 15 2017 Win64" -T v141,host=x64 -- -DXMR-STAK_COMPILE=generic -DCMAKE_LINK_STATIC=ON -DCMAKE_BUILD_TYPE=Release -DMICROHTTPD_ENABLE=ON -DOpenSSL_ENABLE=ON -DCPU_ENABLE=ON -DHWLOC_ENABLE=ON -DOpenCL_ENABLE=OFF -DCUDA_ENABLE=OFF $env:BUILD_SOURCESDIRECTORY\xmr-stak
