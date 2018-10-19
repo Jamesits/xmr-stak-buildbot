94b70fd3ed75ffce51cb3f0b16978abf526f11cb
James Swineson <github@public.swineson.me>
2018-10-19 18:54:49 +0800

    Create build-macos.sh

---
+++ build-macos.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -euv
+
+# install dependencies
+brew install hwloc libmicrohttpd gcc openssl cmake
+
+# get source
+cd $BUILD_SOURCESDIRECTORY
+git clone https://github.com/fireice-uk/xmr-stak.git xmr-stak
+sed -ie "s/2\.0/0\.0/g" xmr-stak/xmrstak/donate-level.hpp
+
+# compile
+cmake . -DOPENSSL_ROOT_DIR=/usr/local/opt/openssl -DXMR-STAK_COMPILE=generic -DCMAKE_LINK_STATIC=ON -DCMAKE_BUILD_TYPE=Release -DMICROHTTPD_ENABLE=ON -DOpenSSL_ENABLE=ON -DCPU_ENABLE=ON -DHWLOC_ENABLE=ON -DOpenCL_ENABLE=ON -DCUDA_ENABLE=OFF
+make -j
+
+# stage artifacts
+mv $BUILD_BINARIESDIRECTORY/bin/* $BUILD_ARTIFACTSTAGINGDIRECTORY
